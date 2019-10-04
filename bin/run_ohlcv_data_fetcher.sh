1b8bb1ba867b86fc2e66e34c17214896b23dd98b
Soren Martius <soeren.martius@gmail.com>
2019-10-04 21:09:36 +0200

    fix bin script to load correct config path

--- bin/run_ohlcv_data_fetcher.sh
+++ bin/run_ohlcv_data_fetcher.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-python3 ${DIR}/../ohlcv/fetch_ohlcv_data.py ${DIR}/../config.toml
+python3 ${DIR}/../ohlcv/fetch_ohlcv_data.py $1
